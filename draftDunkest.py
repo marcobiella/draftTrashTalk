218a9bf1c9ebaeb0bff959caae6369085c160cac
Marco Biella <marcobiella@live.com>
2019-10-23 19:48:39 +0200

    Add files via upload

---
+++ draftDunkest.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 23 19:43:53 2019
+
+@author: marco
+"""
+
+import random as rm
+
+playerList = ["andrea", "marco b", "marco m", "matteo", "mattia", "roberto"]
+
+rm.Random(32).shuffle(playerList)
+
+playerList 
